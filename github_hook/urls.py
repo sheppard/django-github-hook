1502fab6085a44984f593a5bfc847b8ed9cfbe3a
S. Andrew Sheppard <andrew@wq.io>
2013-11-17 19:01:53 -0600

    don't need a router for this app

--- github_hook/urls.py
+++ github_hook/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, url, include
-from rest_framework import routers
 
 from .views import HookView
-
-router = routers.DefaultRouter()
-router.register(r'', HookView(),'hook')
 
 urlpatterns = patterns('',
 	url(r'^(?P<name>[\w-]+)$', HookView.as_view()),
