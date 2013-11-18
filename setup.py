91136e13c7df1f88f322efdeb898122b8408e48c
S. Andrew Sheppard <andrew@wq.io>
2013-11-17 21:31:33 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    :copyright: (assumed) Copyright 2013 by S. Andrew Sheppard
+    :copyright: Copyright 2013 by S. Andrew Sheppard
     :contact: andrew@wq.io
 """
 
 
 from setuptools import setup
+from os.path import join, dirname
 
+LONG_DESCRIPION = """
+Webhooks for GitHub post-receive hooks and other POST requests.
+"""
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return LONG_DESCRIPTION
 
 setup(
     name='django-github-hook',
     version='0.1.0',
-    description='Django GitHub (& Bitbucket) hooks',
-    long_description="Trigger script from POST request (forked by Hugo Geoffroy to add setup.py)",
+    description='Django-powered GitHub (& Bitbucket) web hooks',
+    long_description=long_description(),
     author='S. Andrew Sheppard, Hugo Geoffroy',
     author_email='andrew@wq.io',
     packages = ['github_hook'],
+    install_requires=['Django', 'djangorestframework'],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
     ],
     platforms=['any'],
 )
